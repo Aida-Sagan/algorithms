5ce1a0109e21784df2fa3258edd9b44ed17c6725
Yar <yartor188@gmail.com>
2025-06-21 10:34:00 +0500

    Классовая реализация стека на Python

--- stack.py
+++ stack.py
@@ -4,38 +4,37 @@ class Stack:
 
     def push(self, item):
         self.stack.append(item)
-    
+
     def pop(self):
-        if self.is_emtpy():
+        if self.is_empty():
             return None
-        
         return self.stack.pop()
-    
-    def is_emtpy(self):
+
+
+    def is_empty(self):
         return len(self.stack) == 0
-    
+
     def peek(self):
-        if self.is_emtpy():
+        if self.is_empty():
             return None
         return self.stack[-1]
-    
+
     def size(self):
         return len(self.stack)
-    
-    def __str__(self):
-        return "bottom " + " | ".join(map(str, self.stack)) + " top"
 
-# bottom ... | ... | top
+    def __str__(self):
+        return "bottom " + ' | '.join(map(str, self.stack)) + " top"
+
 stack = Stack()
 stack.push('Python')
 stack.push('Go')
-stack.push("C++")
+stack.push("Rust")
+stack.push('C++')
 
 print(stack)
+print("Top Node" , stack.peek())
+print('Del' , stack.pop())
 
-print('Top element: ', stack.peek())
-print('Delete: ', stack.pop())
-
-print('Top element: ', stack.peek())
-print('Size of stack: ', stack.size())
-print('Stack is empty: ', stack.is_emtpy())
+print("Top Node" , stack.peek())
+print('Size' , stack.size())
+print("Stack is empty", stack.is_empty())
